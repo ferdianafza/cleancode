baa93aa37324f778bb74b7e8199a67c1924b800e
Ferdian Afza <farisafza201002@gmail.com>
2023-11-08 08:20:57 +0700

    Change code on Point.java

--- src/cleancode/Point.java
+++ src/cleancode/Point.java
@@ -8,7 +8,11 @@ package cleancode;
  *
  * @author HP
  */
-public class Point {
-    public double x;
-    public double y;
+public interface Point {
+    double getX();
+    double getY();
+    void setCartesian(double x, double y);
+    double getR();
+    double getTheta();
+    void setPolar(double r, double theta);
 }
