47323343418dcd2e2372c0d7c1bc55c938410c50
Ferdian Afza <farisafza201002@gmail.com>
2023-11-08 07:58:44 +0700

    Change code on Customer.java, rename variables and konstanta readable

--- src/cleancode/Customer.java
+++ src/cleancode/Customer.java
@@ -7,10 +7,8 @@ import java.util.Date;
  * @author HP
  */
 public class Customer {
-    class DtaRcrd102 {
-    private Date genymdhms;
-    private Date modymdhms;
-    private final String pszqint = "102";
-    /* ... */
-    }
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+
 }
