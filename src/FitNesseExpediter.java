0a27f9b4afc3c5281e70883656a8310083fa1cad
Ferdian Afza <farisafza201002@gmail.com>
2023-11-08 08:49:20 +0700

    Change code to Good Format

--- src/FitNesseExpediter.java
+++ src/FitNesseExpediter.java
@@ -8,14 +8,14 @@
  * @author HP
  */
 public class FitNesseExpediter {
-    private   Socket          socket;
-    private   InputStream     input;
-    private   OutputStream    output;
-    private   Request         request;
-    private   Response        response;
-    private   FitNesseContext context;
-    protected long            requestParsingTimeLimit;
-    private   long            requestProgress;
-    private   long            requestParsingDeadline;
-    private   boolean         hasError;
+    private Socket socket;
+    private InputStream input;
+    private OutputStream output;
+    private Request request;
+    private Response response;
+    private FitNesseContext context;
+    protected long requestParsingTimeLimit;
+    private long requestProgress;
+    private long requestParsingDeadline;
+    private boolean hasError;
 }
