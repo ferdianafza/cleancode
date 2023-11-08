9a5be4739789365d24c64dda657c2fc38b6be446
Ferdian Afza <farisafza201002@gmail.com>
2023-11-08 08:45:54 +0700

    Add Sample Bad Formating

---
+++ src/FitNesseExpediter.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class FitNesseExpediter {
+    private   Socket          socket;
+    private   InputStream     input;
+    private   OutputStream    output;
+    private   Request         request;
+    private   Response        response;
+    private   FitNesseContext context;
+    protected long            requestParsingTimeLimit;
+    private   long            requestProgress;
+    private   long            requestParsingDeadline;
+    private   boolean         hasError;
+}
