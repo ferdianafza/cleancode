f8a3ec5c6758f393642841e3275e3b67f6389680
Ferdian Afza <farisafza201002@gmail.com>
2023-11-08 07:49:09 +0700

    change code on Minesweeper.java

--- src/cleancode/Minesweeper.java
+++ src/cleancode/Minesweeper.java
@@ -12,12 +12,15 @@ import java.util.List;
  * @author HP
  */
 public class Minesweeper {
-   public List<int[]> getThem() {
-   List<int[]> list1 = new ArrayList<int[]>();
-       Iterable<int[]> theList = null;
-   for (int[] x : theList)
-   if (x[0] == 4)
-   list1.add(x);
-   return list1;
-   } 
+    public final static int STATUS_VALUE = 0;
+    public final static int FLAGGED = 4;
+    public List<int[]> getFlaggedCells() {
+
+    List<int[]> flaggedCells = new ArrayList<int[]>();
+        Iterable<int[]> gameBoard = null;
+    for (int[] cell : gameBoard)
+    if (cell[STATUS_VALUE] == FLAGGED)
+    flaggedCells.add(cell);
+    return flaggedCells;
+    }
 }
