502259a8463488efe878b76b380a69a653afbdcc
Ferdian Afza <farisafza201002@gmail.com>
2023-11-08 07:56:34 +0700

    add Customer.java

---
+++ src/cleancode/Customer.java
@@ -0,0 +1,16 @@
+package cleancode;
+
+import java.util.Date;
+
+/**
+ *
+ * @author HP
+ */
+public class Customer {
+    class DtaRcrd102 {
+    private Date genymdhms;
+    private Date modymdhms;
+    private final String pszqint = "102";
+    /* ... */
+    }
+}
