b2f0f9f55731afa661fbf191f6cf81c2217d9f59
Ferdian Afza <farisafza201002@gmail.com>
2023-11-08 08:18:49 +0700

    change Cleancode

--- src/cleancode/Cleancode.java
+++ src/cleancode/Cleancode.java
@@ -14,7 +14,7 @@ public class Cleancode {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
